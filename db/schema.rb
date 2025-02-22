d9822eb71c87452cce1f0690651811fdcc49421d
unknown <kunaljha210@gmail.com>
2025-02-22 17:16:40 +0530

    schema changes

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,13 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[8.0].define(version: 2025_02_22_101152) do
+  create_table "posts", force: :cascade do |t|
+    t.string "title"
+    t.text "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -21,14 +28,5 @@ ActiveRecord::Schema[8.0].define(version: 2025_02_22_101152) do
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
-  end
-end
-
-ActiveRecord::Schema[8.0].define(version: 2025_02_22_095425) do
-  create_table "posts", force: :cascade do |t|
-    t.string "title"
-    t.text "content"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
   end
 end
